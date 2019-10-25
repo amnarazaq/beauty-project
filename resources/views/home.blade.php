0817375664cda75cb8fe53612ff4e950a10f846a
Amna Razaq <amna.malik2309@gmail.com>
2019-10-24 22:45:34 -0400

    Add images in home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<div class="container  mt-5 text-center">
+<section id="new-header" style="margin-top: 100px">
     @if (session('status'))
-      <div class="alert alert-success" role="alert">
+        <div class="alert alert-success" role="alert">
             {{ session('status') }}
-       </div>
-     @endif
-     You are logged in!
- </div>
         </div>
-    </div>
-</div>
+    @endif
+
+    	<div class=" container img-fluid">
+			<div class="row d-flex justify-content-center">
+				<div class="banner-image ">
+					<img class="" alt="Get the look"src="{{asset('/images/model.png')}}">
+				</div>
+				<div class="font-weight-bold pl-2 pr-2" style=" font-family: HelveticaNeueW01-77BdCn_692722,arial,sans-serif;
+    font-size: 24px">Get The</div>
+				<div class="banner-image--large">
+	    			<img class="" alt="Get the look"src="{{asset('/images/look.PNG')}}">
+				</div>
+			</div>
+		</div>
+		
+</section>
+<section id="new-body">
+</section>
+
 @stop
 
