7127f5671c55077aae1286a4d09d5d011201d70f
Amna Razaq <amna.malik2309@gmail.com>
2019-11-08 03:05:42 +0000

    create admin dashboard page.

---
+++ resources/views/includes/admin/head.blade.php
@@ -0,0 +1,8 @@
+<base href="./">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<title>Admin Dashboard</title>
+<link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
+<!-- Icons-->
+<link rel="stylesheet" type="text/css" href="{{ mix('/css/coreui-icons.css') }}">
---
+++ resources/views/includes/admin/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                <i class="nav-icon fa fa-tachometer" aria-hidden="true"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="colors.html">
+                <i class="nav-icon fa fa-tags" aria-hidden="true"></i> Brands</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="typography.html">
+                <i class="nav-icon fa fa-th" aria-hidden="true"></i> Categories</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="typography.html">
+                <i class="nav-icon fa fa-product-hunt" aria-hidden="true"></i> Products</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="typography.html">
+                <i class="nav-icon fa fa-cogs fa-3" aria-hidden="true"></i> Settings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="typography.html">
+                <i class="nav-icon fa fa-users" aria-hidden="true"></i> Users</a>
+            </li>
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    </div>
