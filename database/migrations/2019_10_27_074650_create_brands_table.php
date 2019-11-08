71d8c07d19b3298081a9a3a9898fdeb6234a1564
Amna Razaq <amna.malik2309@gmail.com>
2019-11-08 03:06:56 +0000

    Database creation

---
+++ database/migrations/2019_10_27_074650_create_brands_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->string('logo', 50);   
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            ['name' => 'Face Primer'],
+            ['name' => 'Concealer'],
+            ['name' => 'Foundation'],
+            ['name' => 'Face Powder'],
+            ['name' => 'BB Cream'],
+            ['name' => 'Blush'],
+            ['name' => 'Highlight'],
+            ['name' => 'Contour'],
+            ['name' => 'Bronzer'],
+            ['name' => 'Setting Spray'],
+            ['name' => 'Setting Powder'],
+            ['name' => 'Eye Primer'],
+            ['name' => 'Mascara'],
+            ['name' => 'Eyelash Curler'],
+            ['name' => 'Eyelash Glue'],
+            ['name' => 'Extension Eyelashes'],
+            ['name' => 'Eyebrow Pencil'],
+            ['name' => 'Eyebrow Gels'],
+            ['name' => 'Eyebrow Wax'],
+            ['name' => 'Eyebrow Powder'],
+            ['name' => 'Eyeliner'],
+            ['name' => 'Eyeshadow'],
+            ['name' => 'Lipstick'],
+            ['name' => 'Lip Gloss'],
+            ['name' => 'Lip Liner'],
+            ['name' => 'Lip Balm'],
+            ['name' => 'Lip Primer']
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+}
