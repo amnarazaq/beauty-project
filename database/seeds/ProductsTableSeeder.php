5cbd911b9dbb1a30f07dbb6fbc3d070ee7dc87a0
Amna Razaq <amna.malik2309@gmail.com>
2019-11-17 02:15:05 -0500

    Add products seeder

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,6 +11,415 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $products = [
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'EFD6B6',
+                'color_name' => 'Milkshake 100B',
+                'image' => 'milkshake-100b.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'F8ECE1',
+                'color_name' => 'Angel Food 110N',
+                'image' => 'angel-food-110n.png',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'F0CDA9',
+                'color_name' => 'Vanilla 120B',
+                'image' => 'vanilla-120b.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'EFD6B6',
+                'color_name' => 'Panna Cotta 130G',
+                'image' => 'panna-cotta-130g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'EEC494',
+                'color_name' => 'Cashew 140G',
+                'image' => 'cashew-140g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'E8C090',
+                'color_name' => 'Crème Brulée 150G',
+                'image' => 'creme-brulee-150g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'E8C09A',
+                'color_name' => 'Shortbread 200B',
+                'image' => 'shortbread-200b.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'E7BE8E',
+                'color_name' => 'Chai 210B',
+                'image' => 'chai-210b.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'E5B786',
+                'color_name' => 'Custard 220N',
+                'image' => 'custard-220n.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'E3B991',
+                'color_name' => 'Macaroon 230N',
+                'image' => 'macaroon-230n.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'E4B989',
+                'color_name' => 'Toasted Coconut 240N',
+                'image' => 'toasted-coconut-240n.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'DEAF82',
+                'color_name' => 'Cheesecake 250G',
+                'image' => 'cheesecake-250g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'DCB082',
+                'color_name' => 'Latte 300N',
+                'image' => 'latte-300n.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'D9AB73',
+                'color_name' => 'Amaretti 310G',
+                'image' => 'amaretti-310g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'D6A871',
+                'color_name' => 'Tres Leches 320G',
+                'image' => 'tres-leches-320g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'D29F6A',
+                'color_name' => 'Butter Pecan 330N',
+                'image' => 'butter-pecan-330n.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'C99658',
+                'color_name' => 'Baklava 340G',
+                'image' => 'baklava-340g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'C38D54',
+                'color_name' => 'Dulce De Leche 350G',
+                'image' => 'dulce-de-leche-350g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'BF8B4F',
+                'color_name' => 'Macchiato 400G',
+                'image' => 'macchiato-400g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'B58346',
+                'color_name' => 'Brown Sugar 410G',
+                'image' => 'brown-sugar-410g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => 'B98449',
+                'color_name' => 'Toffee 420G',
+                'image' => 'toffee-420g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Fauxfilter Foundation',
+                'category_id' => '3',
+                'brand_id' => '34',
+                'color_code_hex' => '83552F',
+                'color_name' => 'Nutmeg 520G',
+                'image' => 'nutmeg-520g.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'FAC7AA',
+                'color_name' => '201 CLASSIC IVORY',
+                'image' => '201-classic-ivory.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'EDB683',
+                'color_name' => '202 CREAMY NATURAL',
+                'image' => '202-creamy-natural.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'E5B289',
+                'color_name' => '203 NUDE BEIGE',
+                'image' => '203-nude-beige.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'E1AD86',
+                'color_name' => '204 NATURAL BUFF',
+                'image' => '204-natural-buff.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'FAC7AA',
+                'color_name' => '205 NATURAL BEIGE',
+                'image' => '205-natural-beige.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'C99871',
+                'color_name' => '206 BUFF BEIGE',
+                'image' => '206-buff-beige.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'DAA578',
+                'color_name' => '207 SAND BEIGE',
+                'image' => '207-sand-beige.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'CA9351',
+                'color_name' => '209 CARAMEL BEIGE',
+                'image' => '209-caramel-beige.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'Infallible Pro-Glow Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => '974E22',
+                'color_name' => '212 COCOA',
+                'image' => '212-cocoa.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'INFALLIBLE Up to 24HR Fresh Wear Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'F5D7C3',
+                'color_name' => '400 PEARL',
+                'image' => '400-pearl.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'INFALLIBLE Up to 24HR Fresh Wear Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'FCE3D2',
+                'color_name' => '405  PORCELAIN',
+                'image' => '405-porcelain.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'INFALLIBLE Up to 24HR Fresh Wear Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'FAD2B8',
+                'color_name' => '410  IVORY',
+                'image' => '410-ivory.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'INFALLIBLE Up to 24HR Fresh Wear Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'E6B890',
+                'color_name' => '440  NATURAL-ROSE',
+                'image' => '440-natural-rose.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'INFALLIBLE Up to 24HR Fresh Wear Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'E4B083',
+                'color_name' => '445  VANILLA',
+                'image' => '445-vanilla.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+
+            [
+                'name' => 'INFALLIBLE Up to 24HR Fresh Wear Foundation',
+                'category_id' => '3',
+                'brand_id' => '2',
+                'color_code_hex' => 'DEA468',
+                'color_name' => '475 SUN-BEIGE',
+                'image' => '475-sun-beige.jpg',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ]
+        ];
+        DB::table('products')->insert($products);
     }
 }
